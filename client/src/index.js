c882eec81f810d176b978de0e1c6f910a53d814b
Tristan Giardini <tristangiardini@gmail.com>
2023-01-04 11:47:41 -0500

    deleted mobile context.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-import { MobileProvider } from "./MobileContext";
 
 if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <MobileProvider>
-    <App />
-  </MobileProvider>
-);
+root.render(<App />);
